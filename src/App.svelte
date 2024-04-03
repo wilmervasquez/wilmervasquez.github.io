898388e82bd5122d2efd0f08bca2a79542b941e9
wilmervasquez <wilmerluisvasquezlino@gmail.com>
2024-04-03 00:11:40 -0500

    v0.0.3

--- src/App.svelte
+++ src/App.svelte
@@ -1,64 +1,54 @@
 <script lang="ts">
   
-  import { onMount } from 'svelte';
-
-  let initialTime = 30;
-  let currentTime = initialTime;
-  let paragraph:HTMLElement;
-  let input:HTMLInputElement
-  let inputValue:string
-  let letters:InfoLetter[] = []
-  const TEXT = "Hoy fui uno de mis mejores dias y lo aceptamos como regalo amigos mios".toLowerCase()
-
-  interface InfoLetter{
-    letter:string
-    state:"vacio" | "correct" | "incorrect" 
-  }
-  onMount(()=>{
-    input.focus()
-  })
-  function gameOver() {
-    
-  }
-  function initGame() {
-    letters = TEXT.split('').map((l):InfoLetter => {return {letter:l,state:"vacio"}})
-  }
-  initGame()
-  let letterPosition = 0;
-  const intervalId = setInterval(()=>{
-    currentTime--;
-    if (currentTime === 0) {
-      clearInterval(intervalId)
-      console.log("Game Over")
-      gameOver()
-    }
-  },1000)
-  
-  function onKeyUp() {
-    letterPosition = inputValue.length
-
-    let letterOfInputValue = inputValue.split('')
-    for (let i = 0; i < letterOfInputValue.length; i++) {
-      if (letterOfInputValue[i] === letters[i].letter) {
-        letters[i].state = "correct"
-      }else{
-        letters[i].state = "incorrect"
-        
-      }
-      
-    }
-  }
-  function onKeyDown() {
-    letterPosition = input.value.length 
-    
-  }
 </script>
 
 <main>
   <section>
-    <time datetime="">{currentTime}</time>
-    <p>My Portfolio</p>
-    <input type="text" bind:value={inputValue} bind:this={input} on:keydown={onKeyDown} on:keyup={onKeyUp}>
+    <div class="flex gap-4 p-4">
+      <div class="w-40 h-40 bg-gray-600 rounded-2xl flex-shrink-0">
+        <img src="" alt="">
+      </div>
+      <div class="flex flex-col justify-between">
+        <div class="flex justify-between">
+          <div class="flex items-center gap-3 bg-green-800 pr-4 pl-2 text-sm text-green-400 rounded-3xl">
+            <div class="w-2 h-2 bg-green-400 rounded-2xl"></div>
+            <p>Avaible for work</p>
+          </div>
+          <div class="flex cursor-pointer">
+            <div class="rounded-lg hover:bg-neutral-600 p-1">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="#aaa" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"/></svg>
+            </div>
+            <div class="rounded-lg hover:bg-neutral-600 p-1">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke="#aaa" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M22 4.01c-1 .49-1.98.689-3 .99c-1.121-1.265-2.783-1.335-4.38-.737S11.977 6.323 12 8v1c-3.245.083-6.135-1.395-8-4c0 0-4.182 7.433 4 11c-1.872 1.247-3.739 2.088-6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58-1.04 6.522-3.723 7.651-7.742a13.84 13.84 0 0 0 .497-3.753c0-.249 1.51-2.772 1.818-4.013z"/></svg>
+            </div>
+            <div class="rounded-lg hover:bg-neutral-600 p-1">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke="#aaa" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19c-4.3 1.4-4.3-2.5-6-3m12 5v-3.5c0-1 .1-1.4-.5-2c2.8-.3 5.5-1.4 5.5-6a4.6 4.6 0 0 0-1.3-3.2a4.2 4.2 0 0 0-.1-3.2s-1.1-.3-3.5 1.3a12.3 12.3 0 0 0-6.2 0C6.5 2.8 5.4 3.1 5.4 3.1a4.2 4.2 0 0 0-.1 3.2A4.6 4.6 0 0 0 4 9.5c0 4.6 2.7 5.7 5.5 6c-.6.6-.6 1.2-.5 2V21"/></svg>
+            </div>
+          </div>
+        </div>
+        <h1 class="text-3xl font-bold">Hi there! I'm Wilmer V L!</h1>
+        <p class="text-sm text-gray-300">I'm a versatile creator shaping captivating experiences across diverse design landscapes</p>
+        <div class="flex justify-between">
+          <div class="flex items-center gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke="#aaa" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m21 3l-6.5 18a.55.55 0 0 1-1 0L10 14l-7-3.5a.55.55 0 0 1 0-1z"/></svg>
+            Huanuco, Perú
+          </div>
+          <div class="flex items-center gap-2">
+            More bout Me
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke="#aaa" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-6 6l6-6m-6-6l6 6"/></svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="flex p-3 items-center justify-between">
+
+      <h2 class="">Stack</h2>
+      <div class="flex text-sm">
+        Full Stack
+      </div>
+    </div>
   </section>
 </main>
 
@@ -66,51 +56,12 @@
   main{
     padding: 30px;
     display: grid;
-    place-items: center;
+    grid-template-columns: 1fr;
+    gap: 20px;
+    padding: 20px;
   }
-  p {
- 
-    color: rgb(143, 143, 143);
-    font-family: Cascadia Code;
-    font-size: 20px;
-  
-    flex-wrap: wrap;
-  }
-  time{
-    color: yellowgreen;
-  }
-  input{
-    position: absolute;
-    top: 0;
-  }
-
-  @keyframes blink{
-    0%,25%{
-      opacity: 1;
-    }
-    75%{
-      opacity: 0;
-    }
-  }
-  span {
-    position: relative;
-    
-  }
-  span.active::before{
-    content: "|";
-    color: violet ;
-    position: absolute;
-    left: -55%;
-    animation: 1s blink infinite ease-in-out;
-  }
-  span[data-state="correct"]{
-    color: rgb(238, 238, 238);
-    font-weight: bold;
-
-  }
-  span[data-state="incorrect"]{
-    color: rgb(255, 129, 129);
-    font-weight: bold;
-
+  section{
+    background-color: rgb(56, 56, 56);
+    @apply rounded-2xl;
   }
 </style>
